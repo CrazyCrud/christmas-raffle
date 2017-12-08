b0a1fe411b4b775f53930b808605ccfae11b6c11
Constantin Lehenmeier <constantin.lehenmeier@ur.de>
2017-12-08 17:08:58 +0000

    Added snow module

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,8 @@
+import createSnow from 'js/snow';
+
 document.addEventListener('DOMContentLoaded', () => {
 	// do your setup here
 	var $ = require('jquery');
+	createSnow();
 	console.log('Initialized app');
 });
