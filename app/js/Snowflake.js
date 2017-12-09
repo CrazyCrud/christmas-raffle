6ac5aed963a49d37c0ff5626c65de5252fc856ef
Constantin Lehenmeier <CrazyCrud@users.noreply.github.com>
2017-12-09 17:40:02 +0100

    Created snowflake class skeleton

---
+++ app/js/Snowflake.js
@@ -0,0 +1,18 @@
+export class Snowflake { 
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.radius = 0;
+    this.ySpeed = 0;
+    this.xSpeed = 0;
+    this.isResized = false;
+  }
+  
+  draw() {
+  
+  }
+  
+  update() {
+  
+  }
+}
